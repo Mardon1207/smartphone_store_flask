fe3ced07bf9c461bf010673e580bfbaf2152fe89
Mardon1207 <mardonsultonov8@gmail.com>
2023-08-11 10:56:18 +0500

    db.py

--- db.py
+++ db.py
@@ -8,19 +8,20 @@ class SmartphoneDB:
     
     def brands(self):
         """Returns all brands in the database"""
-        pass
+        return self.db.tables()
     
     def get_smartphone_by_brand(self, brand):
         """Returns all products by brand"""
-        pass
+        print(brand)
+        return self.db.table(brand).all()
     
-    def get_smartphone_by_name(self, name):
+    def get_smartphone_by_name(self, brand,name):
         """Returns a product by name"""
-        pass
+        return self.db.table(brand).search(Query().name==name)
 
-    def get_smartphone_by_price(self, price):
+    def get_smartphone_by_price(self,brand, price):
         """Returns a product by price"""
-        pass
+        return self.db.table(brand).search(Query().price==price)
     
     def add_smartphone(self, smartphone, brand):
         """Adds a product to the database"""
